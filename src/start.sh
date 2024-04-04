568937647d0f10719f7ece8d7b9b868780412ffc
Crumar <dev@crumar.de>
2024-04-04 16:01:08 +0200

    fix parameter and nginx non privigled start

--- src/start.sh
+++ src/start.sh
@@ -4,15 +4,16 @@
 
 IFS='|' read -ra ADDR <<< "$DOMAIN"
 for i in "${ADDR[@]}"; do
-    cat /template/update.php | sed "s/<DOMAIN>/${DOMAIN}/g" > /var/www/html/$DOMAIN.php
-    cat /template/zone | sed "s/<DOMAIN>/${DOMAIN}/g" > /var/lib/bind/d.$DOMAIN
-    cat /template/named.conf.local.zone_config | sed "s/<DOMAIN>/${DOMAIN}/" >> /etc/bind/named.conf.local
+    cat /template/update.php | sed "s/<DOMAIN>/${i}/g" > /var/www/html/$i.php
+    cat /template/zone | sed "s/<DOMAIN>/${i}/g" > /var/lib/bind/d.$i
+    cat /template/named.conf.local.zone_config | sed "s/<DOMAIN>/${i}/" >> /etc/bind/named.conf.local
 done
 
 
 chown -R root:bind /etc/bind
 chown -R bind:bind /var/lib/bind
 chown -R www-data:www-data /var/www
+chown -R www-data:www-data /var/lib/nginx
 
 /etc/init.d/php8.1-fpm start
 /etc/init.d/named start
