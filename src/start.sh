76e83f5e5ba26d6c54fe84aa917681a20dc4ab17
Crumar <dev@crumar.de>
2024-04-04 15:57:53 +0200

    fix file redirect

--- src/start.sh
+++ src/start.sh
@@ -4,8 +4,8 @@
 
 IFS='|' read -ra ADDR <<< "$DOMAIN"
 for i in "${ADDR[@]}"; do
-    cat /template/update.php | sed "s/<DOMAIN>/${DOMAIN}/g" /var/www/html/${DOMAIN}.php
-    cat /template/zone | sed "s/<DOMAIN>/${DOMAIN}/g" /var/lib/bind/d.$DOMAIN
+    cat /template/update.php | sed "s/<DOMAIN>/${DOMAIN}/g" > /var/www/html/$DOMAIN.php
+    cat /template/zone | sed "s/<DOMAIN>/${DOMAIN}/g" > /var/lib/bind/d.$DOMAIN
     cat /template/named.conf.local.zone_config | sed "s/<DOMAIN>/${DOMAIN}/" >> /etc/bind/named.conf.local
 done
 
